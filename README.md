93dbcba0c6be384f5a2235742330550c316322f0
Thorsten M <thoto@thoto.net>
2018-04-26 21:23:28 +0200

    minor changes
     - updated readme
     - display password prompt
     - use query-io
     - cleaned up a bit

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Crawls your WUSEL timetable and compiles a single iCalendar file.
 
+iCal files are directly provided by WUSEL. This project just simplifies the
+download procedure eliminating the need to click through every lecture and
+download its ical file by hand.
+
 Later it will be possible to supply a CalDAV target where your timetable
 wile be uploaded.
 
@@ -9,18 +13,19 @@ wile be uploaded.
 
 Invoke using `./singlefile.lisp <matrikelnummer>`. You will be asked for your
 WUSEL password on stdin. The characters of your password will not be
-shown while you are typing.
+shown while you are typing. After download you will get a file `plan.ics` in
+your working directory.
 
 ## Requirements
 
-You need to have SBCL, ASDF and DRAKMA
-installed on your system. There are provided in the `sbcl`, `cl-asdf` and
-`cl-drakma` Debian packages.
-
-iCal files are directly provided by WUSEL. This project just simplifies the
-download procedure eliminating the need to click through every lecture and
-download its ical file by hand.
+You need to have SBCL, ASDF and DRAKMA installed on your system. These
+are provided by the `sbcl`, `cl-asdf` and `cl-drakma` Debian packages.
 
 ## TODO:
  - provide Dockerfile
  - implement CalDAV upload
+
+Notice: this code is quite ugly and parts are somewhat old dating back to
+the time I started attending Uni Wuppertal in 2012. There is also some dead
+code which you don't need. I will clean this up later.
+
--- singlefile.lisp
+++ singlefile.lisp
@@ -4,25 +4,23 @@
 (load "wuseler.lisp")
 
 (defun get-mnr ()
-  "Reads Matrikelnummer from script command line arguments or STDIN"
+  "reads Matrikelnummer from script command line arguments or query-io (stdin)"
   (if (> (length sb-ext:*posix-argv*) 1)
-    (second sb-ext:*posix-argv*)
+    (second sb-ext:*posix-argv*) ; read from command line arguments
     (progn
-      (format t "Matrikelnummer:~%")
-      (read-line))))
+      (format *query-io* "matrikelnummer:~%")
+      (read-line *query-io*))))
 
 (defun get-pw ()
-  "Reads password from stdin while having echo disabled"
-  (let* ((tio-o (sb-posix:tcgetattr 0))
+  "reads password from query-io (stdin) while having echo disabled"
+  (format *query-io* "password:~%")
+  (let* ((tio-o (sb-posix:tcgetattr 0)) ; terminal parametes ...
          (lflags (logand (sb-posix:termios-lflag tio-o) (lognot sb-posix:echo)))
          (tio-n (sb-posix:tcgetattr 0)))
-;    (format t "~a~%" (sb-posix:termios-lflag (sb-posix:tcgetattr 0)))
     (setf (sb-posix:termios-lflag tio-n) lflags)
-    (sb-posix:tcsetattr 0 sb-posix:TCSANOW tio-n) ; disables echo
-;    (format t "~a~%" (sb-posix:termios-lflag (sb-posix:tcgetattr 0)))
-    (let ((res (read-line))) ; reads password here
-      (sb-posix:tcsetattr 0 sb-posix:TCSANOW tio-o) ; enables echo again
-;      (format t "~a~%" (sb-posix:termios-lflag (sb-posix:tcgetattr 0)))
+    (sb-posix:tcsetattr 0 sb-posix:TCSANOW tio-n) ; disable echo
+    (let ((res (read-line *query-io*))) ; read password from user input
+      (sb-posix:tcsetattr 0 sb-posix:TCSANOW tio-o) ; reenable echo
       res)))
 
 
